6d1dfd746561e9c13d53048f58dd4154b3a77c1c
Snowrice <103573547+snowriceDD@users.noreply.github.com>
2023-05-07 22:36:53 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,8 +1,8 @@
 const solution = nl => {
-    let zzak = [];
-    let hole = [];
-    nl.map((e, i) => i % 2 === 0 ? zzak.push(e) : hole.push(e));
-    let zr = zzak.reduce((a, c) => a + c)
-    let hr = hole.reduce((a, c) => a + c)
-    return zr > hr ? zr : hr
+    let zzak = 0;
+    zzak += [...nl].reduce((a, c, i) => (i+1) % 2 === 0 ? a + c : a) + nl[0]
+    let hole = 0;
+    hole += ([...nl].reduce((a, c, i) => (i) % 2 !== 0 ? a + c : a) - (nl[0]))
+    console.log(zzak, hole)
+    return zzak >= hole ? zzak : hole
 }
